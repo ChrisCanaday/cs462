c0d54409c1c6fed35062da8f29c8d6a27bb6e8b6
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-22 19:44:25 -0400

    added randomization back

--- assignments/1/part1/2/main.c
+++ assignments/1/part1/2/main.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
-//#include <math.h>
+#include <time.h>
 #include "mpi.h"
 
 int main(int argc, char **argv ) {
@@ -28,17 +28,25 @@ int main(int argc, char **argv ) {
             char* rand_text = (char*) malloc(size);
 
             if (rank == 0) {
+                srand(time(NULL));
+
+                // randomize the string
+                for (int j = 0; j < size-1; j++) {
+                    rand_text[j] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[rand() % 26];
+                }
+                rand_text[size-1] = '\0';
+
                 start = MPI_Wtime();
                 MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
                 end = MPI_Wtime();
 
                 printf("Process %d took %lf seconds to send message of size %f to process %d\n", 0, end - start, num_bytes, 95);
             }else if (rank == 95) {
-                start = MPI_Wtime();
+                //start = MPI_Wtime();
                 MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-                end = MPI_Wtime();
+                //end = MPI_Wtime();
 
-                printf("Process %d took %lf seconds to receive message of size %f to process %d\n", 95, end - start, num_bytes, status.MPI_SOURCE);
+                //printf("Process %d took %lf seconds to receive message of size %f to process %d\n", 95, end - start, num_bytes, status.MPI_SOURCE);
             }
 
             free(rand_text);
