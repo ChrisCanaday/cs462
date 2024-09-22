f8de0b7514793193bdd5695911b5f8472444ebe7
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-22 18:36:30 -0400

    little big more efficient

--- assignments/1/part1/2/main.c
+++ assignments/1/part1/2/main.c
@@ -19,40 +19,40 @@ int main(int argc, char **argv ) {
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    printf("rank %d || size %d\n", rank, size);
 
     if (size < 96) {
         MPI_Finalize();
         return 1;
     }
 
-    // send messages of size 2 byte -> 1 GB
-    for (int i = 1; i <= 30; i++) {
-        num_bytes = 1 << i;
-        size_t size = (size_t) num_bytes;
-        char* rand_text = (char*) malloc(size);
+    if (rank == 0 || rank == 95) {
+        // send messages of size 2 byte -> 1 GB
+        for (int i = 1; i <= 30; i++) {
+            num_bytes = 1 << i;
+            size_t size = (size_t) num_bytes;
+            char* rand_text = (char*) malloc(size);
 
-        if (rank == 0) {
-            MPI_Barrier(MPI_COMM_WORLD);
+            if (rank == 0) {
+                MPI_Barrier(MPI_COMM_WORLD);
 
-            start = MPI_Wtime();
-            MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
-            end = MPI_Wtime();
+                start = MPI_Wtime();
+                MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
+                end = MPI_Wtime();
 
-            printf("Process %d took %lf seconds to send message of size %f to process %d\n", 0, end - start, num_bytes, 95);
-        }else if (rank == 95) {
-            MPI_Barrier(MPI_COMM_WORLD);
+                printf("Process %d took %lf seconds to send message of size %f to process %d\n", 0, end - start, num_bytes, 95);
+            }else if (rank == 95) {
+                MPI_Barrier(MPI_COMM_WORLD);
 
-            start = MPI_Wtime();
-            MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-            end = MPI_Wtime();
+                start = MPI_Wtime();
+                MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+                end = MPI_Wtime();
 
-            printf("Process %d took %lf seconds to receive message of size %f to process %d\n", 95, end - start, num_bytes, status.MPI_SOURCE);
+                printf("Process %d took %lf seconds to receive message of size %f to process %d\n", 95, end - start, num_bytes, status.MPI_SOURCE);
+            }
+
+            free(rand_text);
         }
-
-        free(rand_text);
     }
-
 
     MPI_Finalize();
     return 0;
