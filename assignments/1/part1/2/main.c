e6e8d651ea04f2af7386ba29b778e9aaa426945e
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-22 20:27:19 -0400

    remove reps

--- assignments/1/part1/2/main.c
+++ assignments/1/part1/2/main.c
@@ -20,32 +20,30 @@ int main(int argc, char **argv ) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == 0 || rank == 95) {
-        for (int reps = 0; reps < 2; reps++){
-            // send messages of size 2 byte -> 1 GB
-            for (int i = 1; i <= 30; i++) {
-                size_t size = 1 << i;
-                char* rand_text = (char*) malloc(size);
+        // send messages of size 2 byte -> 1 GB
+        for (int i = 1; i <= 30; i++) {
+            size_t size = 1 << i;
+            char* rand_text = (char*) malloc(size);
 
-                if (rank == 0) {
-                    srand(time(NULL));
+            if (rank == 0) {
+                srand(time(NULL));
 
-                    // randomize the string
-                    for (int j = 0; j < size-1; j++) {
-                        rand_text[j] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[rand() % 26];
-                    }
-                    rand_text[size-1] = '\0';
-
-                    start = MPI_Wtime();
-                    MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
-                    end = MPI_Wtime();
-
-                    printf("%lf,%lu\n", end - start, size);
-                }else if (rank == 95) {
-                    MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+                // randomize the string
+                for (int j = 0; j < size-1; j++) {
+                    rand_text[j] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[rand() % 26];
                 }
+                rand_text[size-1] = '\0';
 
-                free(rand_text);
+                start = MPI_Wtime();
+                MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
+                end = MPI_Wtime();
+
+                printf("%lf,%lu\n", end - start, size);
+            }else if (rank == 95) {
+                MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
             }
+
+            free(rand_text);
         }
     }
     MPI_Barrier(MPI_COMM_WORLD);
