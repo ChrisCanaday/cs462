87e3e968a507bcb03ca4f382518c1dd91553ebfd
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-22 22:32:07 -0400

    testing part 2

--- assignments/1/part2/main.c
+++ assignments/1/part2/main.c
@@ -1,6 +1,6 @@
 //  Christopher Canaday
 //  COSC 462 Assignment 1
-// Point-to-Point Communication Part 2
+// Collective Communication
 
 #include <stdio.h>
 #include <string.h>
@@ -10,7 +10,6 @@
 
 int main(int argc, char **argv ) {
     int rank, size;
-    double start, end;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -18,9 +17,64 @@ int main(int argc, char **argv ) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    
+    int unique_int = rank + 1;
+    int *the_ints = NULL;
+    int sum = 0;
+    int product = 0;
 
+    if (rank == 0) {
+        the_ints = malloc(size*sizeof(int));
+    }
 
+    MPI_Gather(&unique_int, 1, MPI_INT, the_ints, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("MPI_Gather | Rank 0 has integers:");
+        for(int i = 0; i < size; i++) {
+            printf(" %d", the_ints[i]);
+        }
+        printf("\n");
+
+        free(the_ints);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    the_ints = malloc(size*sizeof(int));
+    MPI_Allgather(&unique_int, 1, MPI_INT, the_ints, 1, MPI_INT, MPI_COMM_WORLD);
+
+    printf("MPI_Allgather |Rank %d has integers:", rank);
+    for(int i = 0; i < size; i++) {
+        printf(" %d", the_ints[i]);
+    }
+    printf("\n");
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Reduce(&unique_int, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("MPI_Reduce | Rank 0 sum: %d\n", sum);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Reduce(&unique_int, &product, 1, MPI_INT, MPI_PROD, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("MPI_Reduce | Rank 0 prod: %d\n", product);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Allreduce(&unique_int, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    printf("MPI_Allreduce | Rank %d sum: %d\n", rank, sum);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Allreduce(&unique_int, &product, 1, MPI_INT, MPI_PROD, MPI_COMM_WORLD);
+    printf("MPI_Allreduce | Rank %d prod: %d\n", rank, product);
+
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
 }
--- assignments/1/part2/sub.sh
+++ assignments/1/part2/sub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=Assignment_1_P1.2
+#SBATCH --job-name=Assignment_1_P2
 #SBATCH --error=job.e%J
 #SBATCH --output=job.o%J
 #
@@ -12,16 +12,16 @@
 #SBATCH --qos=campus
 #
 # Number of MPI tasks needed for use case (example):
-#SBATCH --ntasks=96
+#SBATCH --ntasks=8
 #
 # Processors per task:
 #SBATCH --cpus-per-task=1
 #
 ## Number nodes
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #
 # Wall clock limit:
-#SBATCH --time=00:10:00
+#SBATCH --time=00:01:00
 #
 ## Command(s) to run (example):
 srun ./a.out
