6f4195f7dc604c961e5e87e4c1444cc0e2ee74db
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-22 21:02:10 -0400

    fix

--- assignments/1/part1/2/main.c
+++ assignments/1/part1/2/main.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv ) {
                 MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
                 end = MPI_Wtime();
 
-                printf("Process %d took %lf seconds to send message of size %f to process %d\n", 0, end - start, num_bytes, 95);
+                printf("Process %d took %lf seconds to send message of size %f to process %d\n", 0, end - start, size, 95);
             }else if (rank == 95) {
                 MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
             }
