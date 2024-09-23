51e73b832b5b25b2fccf856c97ed2a747a9f69a1
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-22 21:14:26 -0400

    added reps back

--- assignments/1/part1/2/main.c
+++ assignments/1/part1/2/main.c
@@ -19,26 +19,28 @@ int main(int argc, char **argv ) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == 0 || rank == 95) {
-        // send messages of size 2 byte -> 1 GB
-        for (int i = 1; i <= 30; i++) {
-            size_t size = 1 << i;
-            char* rand_text = (char*) malloc(size);
+        for (int reps = 0; reps < 50; reps++) {
+            // send messages of size 2 byte -> 1 GB
+            for (int i = 1; i <= 30; i++) {
+                size_t size = 1 << i;
+                char* rand_text = (char*) malloc(size);
 
-            if (rank == 0) {
+                if (rank == 0) {
 
-                start = MPI_Wtime();
-                MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
-                end = MPI_Wtime();
+                    start = MPI_Wtime();
+                    MPI_Send(rand_text, size, MPI_CHAR, 95, 0, MPI_COMM_WORLD);
+                    end = MPI_Wtime();
 
-                printf("%lf, %ld\n", end - start, size);
-                //printf("Process %d took %lf seconds to send message of size %ld to process %d\n", 0, end - start, size, 95);
-            }else if (rank == 95) {
-                //start = MPI_Wtime();
-                MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-                //end = MPI_Wtime();
+                    printf("%lf, %ld\n", end - start, size);
+                    //printf("Process %d took %lf seconds to send message of size %ld to process %d\n", 0, end - start, size, 95);
+                }else if (rank == 95) {
+                    //start = MPI_Wtime();
+                    MPI_Recv(rand_text, size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+                    //end = MPI_Wtime();
+                }
+
+                free(rand_text);
             }
-
-            free(rand_text);
         }
     }
 
