ddc4531e0dd9d3601f06bd64deeea88939e5bd8b
Christopher L Canaday <chrislcanaday@gmail.com>
2024-09-25 23:13:26 -0400

    fixed graphing and got rid of unecessary files

--- assignments/1/part1/2/graph.py
+++ assignments/1/part1/2/graph.py
@@ -5,60 +5,41 @@ from matplotlib import pyplot as plt
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 columns = ["Time", "Message_size"]
-print('reading job.osecond')
-df = pd.read_csv("job.osecond", usecols=columns)
+print('reading job.othird')
+df = pd.read_csv("job.othird", usecols=columns)
 
-end = 11
+end = 12
 
 
 thelist = []
 for i in range(0, end):
     thelist.append(df['Time'][i::end].mean())
 
-df['Msize'] = df['Message_size'][0:end]
-
-sizes = df['Message_size'][0:end].to_numpy()
-
-print(sizes)
-list2 = []
-
 laterlist = thelist
 
-for i in range(end-2,end):
-    list = [1, sizes[i]*8]
-    list2.append(list)
-
-x = np.linalg.solve(list2, thelist[end-2:end])
-
-print(x)
-
-print(f'sizes2 {list2}')
 for i in range(len(thelist), len(df["Time"])):
     thelist.append(None)
 
 df["Average"] = thelist
 
-print("Contents in csv file:\n", df)
-
-
-
+# graph config
 plt.xscale("log")
 plt.title("Tcomm vs m")
 plt.ylabel("Time (seconds)")
 plt.xlabel("Message size (bytes)")
+
+# graph the scatter plot
 plt.scatter(df['Message_size'][0:end], df['Average'][0:end])
 
-print(f"len1: {len(df['Message_size'][0:end].to_numpy())}")
-print(f'len2: {len(laterlist[0:end])}')
-
+# get line of best fit and plot it
 a, b = np.polyfit(df['Message_size'][0:end].to_numpy(), laterlist[0:end], 1)
 plt.plot(df['Message_size'][0:end], a * df['Message_size'][0:end].to_numpy() + b)
 
-print(f"a={a} b={b} 1/b={1/b}")
+# print out our calculated tau, mu, and transfer rate
+print(f"MU={a} TAU={b} TRANSFER_RATE={1/a} Bytes ({1/a/1073741824} GiB/s) ({1/a/1073741824*8} Gib/s)")
 
 for xy in zip(df['Message_size'][0:end].to_numpy(), laterlist[0:end]):
     plt.annotate('(%s,%05.5s)' % xy, xy=xy, textcoords='data')
 
 
-#plt.plot(df.Msize, df.Average)
 plt.show()
