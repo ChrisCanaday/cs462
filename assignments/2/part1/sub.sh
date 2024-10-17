9c1b942413d9699c393354bace177cff5715f8d0
Christopher L Canaday <chrislcanaday@gmail.com>
2024-10-17 14:02:15 -0400

    changed structure

---
+++ assignments/2/part1/sub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=Assignment_1_P1.1
+#SBATCH --error=job.e%J
+#SBATCH --output=job.o%J
+#
+# Account
+#SBATCH --account=ISAAC-UTK0319
+#
+# Partition
+#SBATCH --partition=campus
+#SBATCH --qos=campus
+#
+# Number nodes
+#SBATCH --nodes=1
+#
+# CPUs per node
+#SBATCH --ntasks-per-node=8
+#
+# Wall clock limit:
+#SBATCH --time=0-00:01:00
+#
+## Command(s) to run (example):
+srun ./a.out
