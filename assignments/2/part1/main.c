6cce0970c305449e34d365bf003ace0247692fa3
Christopher L Canaday <chrislcanaday@gmail.com>
2024-10-17 18:53:52 -0400

    that fixed it removing prints

--- assignments/2/part1/main.c
+++ assignments/2/part1/main.c
@@ -18,7 +18,6 @@ int main(int argc, char **argv ) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     int vals_per_rank = 64/size;
-    printf("rank %d: vals_per_rank = %d\n", rank, vals_per_rank);
     local_as = calloc(vals_per_rank, sizeof(double));
 
     if (rank == 0) {
@@ -28,7 +27,6 @@ int main(int argc, char **argv ) {
         srand(0);
         for (i = 0; i < 64; i++) {
             a[i] = (double)rand()/RAND_MAX;
-            printf("rank %d: a[%d] = %.8f\n", rank, i, a[i]);
         }
     }
 
@@ -41,7 +39,6 @@ int main(int argc, char **argv ) {
     double tmp = x;
     for (i = 0; i < vals_per_rank; i++, tmp *= x) {
         xs[i] = tmp;
-        printf("rank %d: xs[%d] = %.8f\n", rank, i, xs[i]);
     }
     
     double max_x = xs[vals_per_rank-1];
@@ -51,8 +48,6 @@ int main(int argc, char **argv ) {
     // aka the value of x that will become the basis for ours
     MPI_Exscan(&max_x, &x_to_mult, 1, MPI_DOUBLE, MPI_PROD, MPI_COMM_WORLD);
 
-    printf("rank %d: x_to_mult = %f\n", rank, x_to_mult);
-
     // multiply to make the values correct
     if (rank != 0) {
         for (i = 0; i < vals_per_rank; i++) {
@@ -60,14 +55,11 @@ int main(int argc, char **argv ) {
         }
     }
 
-
     // multiply by proper random value and sum
     double local_sum = 0;
     for (i = 0; i < vals_per_rank; i++) {
         local_sum += local_as[i] * xs[i];
     }
-
-    printf("rank %d: local_sum = %.8f\n", rank, local_sum);
 
     // MPI_Reduce
     double total_sum = 0;
