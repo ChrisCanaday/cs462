0c7b599d4bfc5634c83772a1d72bd9ab1ba263dd
Christopher L Canaday <chrislcanaday@gmail.com>
2024-10-18 15:25:31 -0400

    working code for part2 just gotta do all the tests

--- assignments/2/part2/main.c
+++ assignments/2/part2/main.c
@@ -16,12 +16,39 @@ void main_serial()
         sum += 4.0/(1.0+x*x);
     }
     double pi = sum*step;
-    printf("The value of pi = %f\n",pi);
+    printf("The value of pi = %.8f\n\t%.8f * %.8f\n",pi, sum, step);
 }
 
 
 int main(int argc, char **argv ) {
     MPI_Init(&argc, &argv);
+    int rank, size, i;
+    long steps = 10000000;
+    double step = 1/(double)steps;
+    double x, sum=0.0;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        main_serial();
+    }
+
+    int steps_per_proc = steps / size;
+
+    for (i = 0; i < steps_per_proc; i++) {
+        x = ((i+rank*steps_per_proc)+.5)*step;
+        sum += 4.0/(1.0+x*x);
+    }
+
+    double total_sum = 0.0;
+    MPI_Reduce(&sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        double pi = total_sum * step;
+        printf("total_sum = %.8f\n\t %.8f * %.8f\n", pi, total_sum, step);
+    }
+
 
     MPI_Finalize();
     return 0;
