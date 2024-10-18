1cc2f529da1f8f5979bdc60882fa80ce57fbe4c1
Christopher L Canaday <chrislcanaday@gmail.com>
2024-10-17 22:00:26 -0400

    default part2 start

--- assignments/2/README.md
+++ assignments/2/README.md
@@ -1,2 +1,9 @@
 # Assignment 2
 
+Part 1 is #1
+
+Part 2 is #2
+
+Job results directory contains the result of all jobs ran on isaac.
+
+scripts are the SLURM script. p stands for processor.
--- assignments/2/part2/main.c
+++ assignments/2/part2/main.c
@@ -5,6 +5,20 @@
 #include <stdbool.h>
 #include "mpi.h"
 
+#define nsteps 10000000
+void main_serial()
+{
+    double x, sum=0.0;
+    double step = 1.0/nsteps;
+    for (int i=0; i<nsteps; i++)
+    {
+        x = (i+0.5)*step;
+        sum += 4.0/(1.0+x*x);
+    }
+    double pi = sum*step;
+    printf("The value of pi = %f\n",pi);
+}
+
 
 int main(int argc, char **argv ) {
     MPI_Init(&argc, &argv);
