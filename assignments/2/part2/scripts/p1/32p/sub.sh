5426a463d24ad31135ebd7112d71fc17281adf2a
Christopher L Canaday <chrislcanaday@gmail.com>
2024-10-19 16:46:27 -0400

    change scripts back and let them sit for a while hopefully they run

--- assignments/2/part2/scripts/p1/32p/sub.sh
+++ assignments/2/part2/scripts/p1/32p/sub.sh
@@ -11,11 +11,11 @@
 #SBATCH --partition=campus
 #SBATCH --qos=campus
 #
-# Number of MPI tasks needed for use case (example):
-#SBATCH --ntasks=32
+# Number nodes
+#SBATCH --nodes=1
 #
-# Processors per task:
-#SBATCH --cpus-per-task=1
+# CPUs per node
+#SBATCH --ntasks-per-node=32
 #
 # Wall clock limit:
 #SBATCH --time=0-00:01:00
--- assignments/2/part2/scripts/p2/6/sub.sh
+++ assignments/2/part2/scripts/p2/6/sub.sh
@@ -11,11 +11,11 @@
 #SBATCH --partition=campus
 #SBATCH --qos=campus
 #
-# Number of MPI tasks needed for use case (example):
-#SBATCH --ntasks=16
+# Number nodes
+#SBATCH --nodes=1
 #
-# Processors per task:
-#SBATCH --cpus-per-task=1
+# CPUs per node
+#SBATCH --ntasks-per-node=16
 #
 # Wall clock limit:
 #SBATCH --time=0-00:01:00
