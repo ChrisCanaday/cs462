14b64214f330a29acbfe192053cd91cca1b3d107
Christopher L Canaday <chrislcanaday@gmail.com>
2024-11-26 20:31:52 -0500

    added 512 proc

---
+++ assignments/term/part2/scripts/8x8x8.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=Assignment_T_P2
+#SBATCH --error=job.e%J
+#SBATCH --output=job.o%J
+#
+# Account
+#SBATCH --account=ISAAC-UTK0319
+#
+# Partition
+#SBATCH --partition=campus
+#SBATCH --qos=campus
+#
+# Number tasks
+#SBATCH --ntasks=512
+#
+# Wall clock limit:
+#SBATCH --time=0-00:01:00
+#
+## Command(s) to run (example):
+srun ./a.out -n 1024 -P 512
